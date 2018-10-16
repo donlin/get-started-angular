ab33378fa983f5199da06f4127e39d0e4c1e4c19
Don Lin <don@lemonade.com.au>
2018-10-16 17:00:00 +1100

    Fix unittest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        NgbModule.forRoot(),
+      ],
       declarations: [AppComponent]
     }).compileComponents();
   }));
